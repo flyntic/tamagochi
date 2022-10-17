8e7471d731cbe962b61f1d8531d77148faf60eae
flyntic <64376713+flyntic@users.noreply.github.com>
2022-10-17 21:29:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tamagochi
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Tamagochi t = new Tamagochi();
+        }
+    }
+}   
---
+++ Tamagochi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTamagochi
+{
+    class Tamagochi
+    {
+        private string Name { get; set; }
+        string wishToEat() => "Хочу есть";
+        string wishToWalk() => "Хочу гулять";
+        string wishToSleep() => "Хочу спать";
+
+        const List<var> all_wishes= new List<var>[3] { wishToEat, wishToWalk, wishToSleep };
+
+        aWish wish;
+        public override string ToString() 
+        {
+            string str = "Я - " + Name;
+            if (wish == null) return (str + " Желаний нет! ");
+            foreach (aWish a in wish.GetInvocationList())
+                str += "\n" + a;
+            return str;
+          
+        }
+
+         
+        public bool isLeave()
+        {
+            if (wish.GetInvocationList().Length > 3) return false;
+            return true;
+        }
+
+
+        public Tamagochi()
+        {
+            Name = "1";
+          //  all_wiches = new List<aWish>{ ()=>"Хочу есть", ()=>"Хочу гулять",()=>"Хочу спать"};
+        }
+    }
+
+    delegate string aWish();
+}
